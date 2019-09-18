6ccf7b5caef6497d949063bd806d74395f1ab721
Kip Cole <kipcole9@gmail.com>
2019-09-19 03:57:24 +0800

    Progress on basic parsing, lots of refactoring required

--- lib/combinators.ex
+++ lib/combinators.ex
@@ -1,18 +1,196 @@
 defmodule Snowball.Combinators do
+  import NimbleParsec
+
+  def whitespace do
+    ascii_string([?\s, ?\t, ?\n], min: 1)
+    |> label("whitespace")
+  end
+
+  # <plus_or_minus> ::= + || -
+  def plus_or_minus do
+    ignore(optional(whitespace()))
+    |> ascii_char([?+, ?-])
+    |> ignore(optional(whitespace()))
+  end
+
+  def r_paren do
+    ascii_char([?)])
+    |> label("right parenthesis")
+  end
+
+  def l_paren do
+    ascii_char([?(])
+    |> label("left parenthesis")
+  end
 
   # <letter>        ::= a || b || ... || z || A || B || ... || Z
+  def letter do
+    ascii_char([?a..?z, ?A..?Z])
+    |> label("alphetic character")
+  end
+
   # <digit>         ::= 0 || 1 || ... || 9
+  def digit do
+    ascii_char([?0..?9])
+    |> label("digit")
+  end
+
+  def underscore do
+    ascii_char([?_])
+    |> label("underscore")
+  end
+
+  def single_quote do
+    ascii_char([?'])
+    |> label("single quote")
+  end
+
+  def equals do
+    ascii_char([?=])
+  end
+
+  def plus_equals do
+    string("+=")
+  end
+
+  def minus_equals do
+    string("-=")
+  end
+
+  def mult_equals do
+    string("*=")
+  end
+
+  def div_equals do
+    string("/=")
+  end
+
+  def double_equals do
+    string("==")
+  end
+
+  def not_equals do
+    string("!=")
+  end
+
+  def less_than do
+    string("<")
+  end
+
+  def less_than_or_equals do
+    string("<=")
+  end
+
+  def greater_than do
+    string(">")
+  end
+
+  def dollar do
+    ascii_char([?$])
+  end
+
+  def greater_than_or_equals do
+    string(">=")
+  end
+
+  def plus do
+    string("+")
+  end
+
+  def minus do
+    string("-")
+  end
+
+  def mult do
+    string("*")
+  end
+
+  def div do
+    string("/")
+  end
+
+  def l_bracket do
+    string("[")
+  end
+
+  def r_bracket do
+    string("]")
+  end
+
   # <name>          ::= <letter> [ <letter> || <digit> || _ ]*
+  def name do
+    letter()
+    |> repeat(choice([letter(), digit(), underscore()]))
+    |> reduce({List, :to_string, []})
+    |> reduce(:to_ast)
+  end
+
   # <s_name>        ::= <name>
+  def s_name do
+    name()
+    |> label("s_name")
+  end
+
   # <i_name>        ::= <name>
+  def i_name do
+    name()
+    |> label("integer variable name")
+  end
+
   # <b_name>        ::= <name>
+  def b_name do
+    name()
+    |> label("b_name")
+  end
+
   # <r_name>        ::= <name>
+  def r_name do
+    name()
+    |> label("r_name")
+  end
+
   # <g_name>        ::= <name>
+  def g_name do
+    name()
+    |> label("g_name")
+  end
+
   # <literal string>::= '[<char>]*'
+  def literal_string do
+    ignore(single_quote())
+    |> repeat(
+      choice([
+        utf8_string([{:not, ?'}, {:not, ?{}], min: 1),
+        ignore(ascii_char([?{])) |> utf8_string([{:not, ?}}], min: 1) |> ignore(ascii_char([?}]))
+      ])
+    )
+    |> ignore(single_quote())
+    |> reduce({Enum, :join, []})
+  end
+
   # <number>        ::= <digit> [ <digit> ]*
+  def number do
+    digit()
+    |> repeat(digit())
+  end
+
   #
   # S               ::= <s_name> || <literal string>
+  def s do
+    choice([
+      s_name(),
+      literal_string()
+    ])
+  end
+
   # G               ::= <g_name> || <literal string>
+  def g do
+    choice([
+      g_name(),
+      literal_string()
+    ])
+  end
+
   #
   # <declaration>   ::= strings ( [<s_name>]* ) ||
   #                     integers ( [<i_name>]* ) ||
@@ -20,52 +198,348 @@ defmodule Snowball.Combinators do
   #                     routines ( [<r_name>]* ) ||
   #                     externals ( [<r_name>]* ) ||
   #                     groupings ( [<g_name>]* )
-  #
+  def declaration do
+    choice([
+      string_declarations(),
+      integer_declarations(),
+      boolean_declarations(),
+      routine_declarations(),
+      external_declarations(),
+      groupings_declarations()
+    ])
+  end
+
+  def string_declarations do
+    string("strings")
+    |> ignore(optional(whitespace()))
+    |> ignore(l_paren())
+    |> repeat(ignore(optional(whitespace())) |> concat(s_name()))
+    |> ignore(optional(whitespace()))
+    |> ignore(r_paren())
+    |> reduce(:wrap_declarations)
+    |> label("string declarations")
+  end
+
+  def integer_declarations do
+    string("integers")
+    |> ignore(optional(whitespace()))
+    |> ignore(l_paren())
+    |> repeat(ignore(optional(whitespace())) |> concat(i_name()))
+    |> ignore(optional(whitespace()))
+    |> ignore(r_paren())
+    |> reduce(:wrap_declarations)
+    |> label("integer declarations")
+  end
+
+  def boolean_declarations do
+    string("booleans")
+    |> ignore(optional(whitespace()))
+    |> ignore(l_paren())
+    |> repeat(ignore(optional(whitespace())) |> concat(b_name()))
+    |> ignore(optional(whitespace()))
+    |> ignore(r_paren())
+    |> reduce(:wrap_declarations)
+    |> label("boolean declarations")
+  end
+
+  def routine_declarations do
+    string("routines")
+    |> ignore(optional(whitespace()))
+    |> ignore(l_paren())
+    |> repeat(ignore(optional(whitespace())) |> concat(r_name()))
+    |> ignore(optional(whitespace()))
+    |> ignore(r_paren())
+    |> reduce(:wrap_declarations)
+    |> label("routine declarations")
+  end
+
+  def external_declarations do
+    string("externals")
+    |> ignore(optional(whitespace()))
+    |> ignore(l_paren())
+    |> repeat(ignore(optional(whitespace())) |> concat(r_name()))
+    |> ignore(optional(whitespace()))
+    |> ignore(r_paren())
+    |> reduce(:wrap_declarations)
+    |> label("external declarations")
+  end
+
+  def groupings_declarations do
+    string("groupings")
+    |> ignore(optional(whitespace()))
+    |> ignore(l_paren())
+    |> repeat(ignore(optional(whitespace())) |> concat(g_name()))
+    |> ignore(optional(whitespace()))
+    |> ignore(r_paren())
+    |> reduce(:wrap_declarations)
+    |> label("groupings declarations")
+  end
+
+  def wrap_declarations([type | rest]) do
+    {:declare, [], [String.to_atom(type), rest]}
+  end
+
   # <r_definition>  ::= define <r_name> as C
-  # <plus_or_minus> ::= + || -
+  def r_definition do
+    string("define")
+    |> ignore(whitespace())
+    |> concat(r_name())
+    |> ignore(whitespace())
+    |> string("as")
+    |> ignore(whitespace())
+    |> parsec(:c)
+  end
+
   # <g_definition>  ::= define <g_name> G [ <plus_or_minus> G ]*
-  #
-  # AE              ::= (AE) ||
-  #                     AE + AE || AE - AE || AE * AE || AE / AE || - AE ||
-  #                     maxint || minint || cursor || limit ||
-  #                     size || sizeof <s_name> ||
-  #                     len || lenof <s_name> ||
-  #                     <i_name> || <number>
-  #
+  def g_definition do
+    string("define")
+    |> ignore(whitespace())
+    |> concat(g_name())
+    |> ignore(whitespace())
+    |> concat(g())
+    |> repeat(plus_or_minus() |> concat(g()))
+  end
+
   # <i_assign>      ::= $ <i_name> = AE ||
   #                     $ <i_name> += AE || $ <i_name> -= AE ||
   #                     $ <i_name> *= AE || $ <i_name> /= AE
-  #
+  def i_assign do
+    choice([
+      ignore(dollar())
+      |> concat(i_name())
+      |> ignore(optional(whitespace()))
+      |> concat(equals())
+      |> ignore(optional(whitespace()))
+      |> parsec(:ae),
+      ignore(dollar())
+      |> concat(i_name())
+      |> ignore(optional(whitespace()))
+      |> concat(plus_equals())
+      |> parsec(:ae),
+      ignore(dollar())
+      |> concat(i_name())
+      |> ignore(optional(whitespace()))
+      |> concat(minus_equals())
+      |> ignore(optional(whitespace()))
+      |> parsec(:ae),
+      ignore(dollar())
+      |> concat(i_name())
+      |> ignore(optional(whitespace()))
+      |> concat(mult_equals())
+      |> ignore(optional(whitespace()))
+      |> parsec(:ae),
+      ignore(dollar())
+      |> concat(i_name())
+      |> ignore(optional(whitespace()))
+      |> concat(div_equals())
+      |> ignore(optional(whitespace()))
+      |> parsec(:ae)
+    ])
+    |> reduce(:to_ast)
+  end
+
   # <i_test_op>     ::= == || != || > || >= || < || <=
-  #
+  def i_test_op do
+    choice([
+      double_equals(),
+      equals(),
+      not_equals(),
+      greater_than_or_equals(),
+      greater_than(),
+      less_than_or_equals(),
+      less_than()
+    ])
+  end
+
   # <i_test>        ::= $ ( AE <i_test_op> AE ) ||
   #                     $ <i_name> <i_test_op> AE
-  #
+  def i_test do
+    choice([
+      dollar()
+      |> ignore(optional(whitespace()))
+      |> concat(l_paren())
+      |> ignore(optional(whitespace()))
+      |> parsec(:ae)
+      |> ignore(optional(whitespace()))
+      |> concat(i_test_op())
+      |> ignore(optional(whitespace()))
+      |> parsec(:ae)
+      |> ignore(optional(whitespace()))
+      |> concat(r_paren()),
+      dollar()
+      |> ignore(optional(whitespace()))
+      |> concat(i_name())
+      |> ignore(optional(whitespace()))
+      |> concat(i_test_op())
+      |> ignore(optional(whitespace()))
+      |> parsec(:ae)
+    ])
+  end
+
   # <s_command>     ::= $ <s_name> C
-  #
-  # C               ::= ( [C]* ) ||
-  #                     <i_assign> || <i_test> || <s_command> || C or C || C and C ||
-  #                     not C || test C || try C || do C || fail C ||
-  #                     goto C || gopast C || repeat C || loop AE C ||
-  #                     atleast AE C || S || = S || insert S || attach S ||
-  #                     <- S || delete ||  hop AE || next ||
-  #                     => <s_name> || [ || ] || -> <s_name> ||
-  #                     setmark <i_name> || tomark AE || atmark AE ||
-  #                     tolimit || atlimit || setlimit C for C ||
-  #                     backwards C || reverse C || substring ||
-  #                     among ( [<literal string> [<r_name>] || (C)]* ) ||
-  #                     set <b_name> || unset <b_name> || <b_name> ||
-  #                     <r_name> || <g_name> || non [-] <g_name> ||
-  #                     true || false || ?
-  #
-  # P              ::=  [P]* || <declaration> ||
-  #                     <r_definition> || <g_definition> ||
-  #                     backwardmode ( P )
-  #
-  # <program>      ::=  P
+  def s_command do
+    dollar() |> concat(s_name()) |> ignore(optional(whitespace())) |> parsec(:c)
+  end
+
+  def among_expression do
+    literal_string()
+    |> ignore(whitespace())
+    |> repeat(
+      choice([
+        r_name(),
+        l_paren()
+        |> ignore(optional(whitespace()))
+        |> parsec(:c)
+        |> ignore(optional(whitespace()))
+        |> concat(r_paren())
+      ])
+    )
+  end
+
+  def do_ do
+    string("do")
+    |> ignore(whitespace())
+    |> parsec(:c)
+    |> label("do")
+  end
+
+  def set do
+    string("set")
+    |> ignore(whitespace())
+    |> concat(b_name())
+    |> reduce(:to_ast)
+    |> label("set")
+  end
+
+  def unset do
+    string("unset")
+    |> ignore(whitespace())
+    |> concat(b_name())
+    |> reduce(:to_ast)
+    |> label("unset")
+  end
+
+  def not_ do
+    string("not")
+    |> ignore(whitespace())
+    |> parsec(:c)
+    |> reduce(:to_ast)
+    |> label("not")
+  end
+
+  def test do
+    string("test")
+    |> ignore(whitespace())
+    |> parsec(:c)
+    |> reduce(:to_ast)
+    |> label("test")
+  end
+
+  def try_ do
+    string("try")
+    |> ignore(whitespace())
+    |> parsec(:c)
+    |> reduce(:to_ast)
+    |> label("try")
+  end
+
+  def fail do
+    string("fail")
+    |> ignore(whitespace())
+    |> parsec(:c)
+    |> reduce(:to_ast)
+    |> label("fail")
+  end
+
+  def goto do
+    string("goto")
+    |> ignore(whitespace())
+    |> parsec(:c)
+    |> reduce(:to_ast)
+    |> label("goto")
+  end
+
+  def gopast do
+    string("gopast")
+    |> ignore(whitespace())
+    |> parsec(:c)
+    |> reduce(:to_ast)
+    |> label("gopast")
+  end
+
+  def repeat do
+    string("repeat")
+    |> ignore(whitespace())
+    |> parsec(:c)
+    |> reduce(:to_ast)
+    |> label("repeat")
+  end
+
+  def loop do
+    string("loop")
+    |> ignore(whitespace())
+    |> parsec(:ae)
+    |> ignore(optional(whitespace()))
+    |> parsec(:c)
+    |> reduce(:to_ast)
+    |> label("loop")
+  end
+
+  def atleast do
+    string("atleast")
+    |> ignore(whitespace())
+    |> parsec(:ae)
+    |> ignore(optional(whitespace()))
+    |> parsec(:c)
+    |> reduce(:to_ast)
+    |> label("atleast")
+  end
+
+  def backwards do
+    string("backwardsmode")
+    |> ignore(whitespace())
+    |> ignore(l_paren())
+    |> parsec(:c)
+    |> ignore(optional(whitespace()))
+    |> ignore(r_paren())
+    |> label("backwards mode")
+  end
+
+  def delete do
+    string("delete")
+  end
+
+  def string_escapes do
+    ignore(string("stringescapes"))
+    |> ignore(whitespace())
+    |> ascii_char([])
+    |> ascii_char([])
+    |> reduce(:string_escapes)
+  end
+
+  def string_escapes(chars) do
+    {:escape, chars}
+  end
+
+  def to_ast([variable]) do
+    {String.to_atom(variable), [], Elixir}
+  end
+
+  def to_ast([{_, _, _} = left, op, builtin]) when is_binary(builtin) do
+    right = {String.to_atom(builtin), [], []}
+    {List.to_atom([op]), [], [left, right]}
+  end
+
+  def to_ast([{_, _, _} = left, op, {_, _, _} = right]) do
+    {List.to_atom([op]), [], [left, right]}
+  end
+
+  def to_ast([function | args]) do
+    {String.to_atom(function), [], args}
+  end
+
   #
   #
   #
   # synonyms:      <+ for insert
-
 end
--- lib/snowball.ex
+++ lib/snowball.ex
@@ -1,3 +1,199 @@
 defmodule Snowball do
+  import NimbleParsec
+  import Snowball.Combinators
 
+  def parse(input, rule \\ :program) do
+    case apply(__MODULE__, rule, [input]) do
+      {:ok, result, "", _state, {_line, _}, _character} ->
+        {:ok, result}
+
+      {:ok, _result, rest, _state, {line, _}, character} ->
+        {:error,
+         {Snowball.ParseError,
+          "Couldn't parse file after line #{line} character #{character}:\n#{inspect(rest)}"}}
+    end
+  rescue
+    exception in [Snowball.ParseError] ->
+      {:error, {Snowball.ParseError, exception.message}}
+  end
+
+  defparsec :declaration, declaration()
+  defparsec :literal, literal_string()
+
+  defparsec :program,
+            parsec(:p)
+            |> ignore(optional(whitespace()))
+            |> eos()
+
+  # P              ::=  [P]* || <declaration> ||
+  #                     <r_definition> || <g_definition> ||
+  #                     backwardmode ( P )
+  defparsec :p,
+            repeat(
+              ignore(optional(whitespace()))
+              |> choice([
+                declaration(),
+                string_escapes(),
+                r_definition(),
+                g_definition(),
+                backwards()
+              ])
+            )
+
+  #
+  # C               ::= ( [C]* ) ||
+  #                     <i_assign> || <i_test> || <s_command> || C or C || C and C ||
+  #                     not C || test C || try C || do C || fail C ||
+  #                     goto C || gopast C || repeat C || loop AE C ||
+  #                     atleast AE C || S || = S || insert S || attach S ||
+  #                     <- S || delete ||  hop AE || next ||
+  #                     => <s_name> || [ || ] || -> <s_name> ||
+  #                     setmark <i_name> || tomark AE || atmark AE ||
+  #                     tolimit || atlimit || setlimit C for C ||
+  #                     backwards C || reverse C || substring ||
+  #                     among ( [<literal string> [<r_name>] || (C)]* ) ||
+  #                     set <b_name> || unset <b_name> || <b_name> ||
+  #                     <r_name> || <g_name> || non [-] <g_name> ||
+  #                     true || false || ?
+  defparsec(
+    :c,
+    repeat(
+      ignore(optional(whitespace()))
+      |> choice([
+        l_bracket(),
+        r_bracket(),
+        i_assign(),
+        i_test(),
+        s_command(),
+
+        # Built-ins must come before
+        # variable names
+        do_(),
+        set(),
+        unset(),
+        delete(),
+        not_(),
+        test(),
+        try_(),
+        fail(),
+        goto(),
+        gopast(),
+        repeat(),
+        loop(),
+        atleast(),
+        s(),
+
+        # Variable names
+        b_name(),
+        r_name(),
+        g_name(),
+
+        equals() |> ignore(optional(whitespace())) |> concat(s()),
+        string("insert") |> ignore(whitespace()) |> concat(s()),
+        string("attach") |> ignore(whitespace()) |> concat(s()),
+        string("<-") |> ignore(optional(whitespace())) |> concat(s()),
+        string("hop") |> ignore(whitespace()) |> parsec(:ae),
+        string("next"),
+        string("=>") |> ignore(optional(whitespace())) |> concat(s_name()),
+        string("->") |> ignore(optional(whitespace())) |> concat(s_name()),
+        string("setmark") |> ignore(whitespace()) |> concat(i_name()),
+        string("tomark") |> ignore(whitespace()) |> concat(i_name()),
+        string("atmark") |> ignore(whitespace()) |> concat(i_name()),
+        string("tolimit"),
+        string("atlimit"),
+        string("setlimit")
+        |> ignore(whitespace())
+        |> parsec(:c)
+        |> ignore(whitespace())
+        |> string("for")
+        |> ignore(whitespace())
+        |> parsec(:c),
+        string("backwards") |> ignore(whitespace()) |> parsec(:c),
+        string("reverse") |> ignore(whitespace()) |> parsec(:c),
+        string("substring"),
+        string("among")
+        |> ignore(whitespace())
+        |> concat(l_paren())
+        |> ignore(optional(whitespace()))
+        |> concat(among_expression())
+        |> ignore(optional(whitespace()))
+        |> concat(r_paren()),
+
+        string("non")
+        |> ignore(whitespace())
+        |> concat(minus())
+        |> ignore(optional(whitespace()))
+        |> concat(g_name()),
+        string("true"),
+        string("false"),
+        ascii_char([??]),
+
+        # Recursive parts come last
+        ignore(l_paren())
+        |> parsec(:c)
+        |> ignore(optional(whitespace()))
+        |> ignore(r_paren()),
+
+        # parsec(:c)
+        # |> ignore(optional(whitespace()))
+        # |> string("or")
+        # |> ignore(optional(whitespace()))
+        # |> parsec(:c),
+        # parsec(:c)
+        # |> ignore(optional(whitespace()))
+        # |> string("and")
+        # |> ignore(optional(whitespace()))
+        # |> parsec(:c)
+      ])
+    )
+  )
+
+
+
+  # AE              ::= (AE) ||
+  #                     AE + AE || AE - AE || AE * AE || AE / AE || - AE ||
+  #                     maxint || minint || cursor || limit ||
+  #                     size || sizeof <s_name> ||
+  #                     len || lenof <s_name> ||
+  #                     <i_name> || <number>
+  defparsec(
+    :ae,
+    choice([
+      string("maxint"),
+      string("minint"),
+      string("cursor"),
+      string("limit"),
+      string("size"),
+      string("sizeof"),
+      string("lenof") |> ignore(whitespace()) |> concat(s_name()),
+      i_name(),
+      number(),
+      l_paren()
+      |> ignore(optional(whitespace()))
+      |> parsec(:ae)
+      |> optional(whitespace())
+      |> concat(r_paren()),
+      parsec(:ae)
+      |> ignore(optional(whitespace()))
+      |> concat(plus())
+      |> ignore(optional(whitespace()))
+      |> parsec(:ae),
+      parsec(:ae)
+      |> ignore(optional(whitespace()))
+      |> concat(minus())
+      |> ignore(optional(whitespace()))
+      |> parsec(:ae),
+      parsec(:ae)
+      |> ignore(optional(whitespace()))
+      |> concat(mult())
+      |> ignore(optional(whitespace()))
+      |> parsec(:ae),
+      parsec(:ae)
+      |> ignore(optional(whitespace()))
+      |> concat(div())
+      |> ignore(optional(whitespace()))
+      |> parsec(:ae)
+    ])
+    |> reduce(:to_ast)
+  )
 end
